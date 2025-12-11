c9b95e70ba2bc0f202e30d3cbf2a66045dbe5c20
ArbabSami <arbabsamii117@gmail.com>
2025-12-11 17:25:46 +0500

    Create main.dart

---
+++ ASSIGNMENT 04/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'screens/form_screen.dart';
+import 'screens/list_screen.dart';
+import 'services/supabase_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SupabaseService.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Submission Form',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FormScreen(),
+        '/list': (context) => const ListScreen(),
+      },
+    );
+  }
+}
